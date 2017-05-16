d1dd3c09d0994880ba5156f90cc25305d9bc6983
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-15 23:26:44 -0400

    Account and checking account class structured

--- abstractClasses/src/main/java/Account.java
+++ abstractClasses/src/main/java/Account.java
@@ -1,5 +1,16 @@
 /**
  * Created by rahmirford on 5/15/17.
  */
-public class Account {
+public abstract class Account {
+    private double balance;
+    private double lastDeposit;
+    private double lastWithdrawal;
+
+    public abstract double getBalance();
+    public abstract double getLastDeposit();
+    public abstract double getLastWithdrawal();
+    public abstract void makeDeposit();
+    public abstract void makeWithdrawal();
+
+
 }
--- abstractClasses/src/main/java/CheckingAccount.java
+++ abstractClasses/src/main/java/CheckingAccount.java
@@ -1,5 +1,26 @@
 /**
  * Created by rahmirford on 5/15/17.
  */
-public class CheckingAccount {
+public class CheckingAccount extends Account{
+    private double balance;
+    private double lastDeposit;
+    private double lastWithdrawal;
+
+    public double getBalance(){
+        return balance;
+    };
+
+    public double getLastDeposit(){
+        return lastDeposit;
+    };
+
+    public double getLastWithdrawal(){
+        return lastWithdrawal;
+    };
+
+    public void makeDeposit(){
+
+    };
+    public void makeWithdrawal(){};
+
 }
